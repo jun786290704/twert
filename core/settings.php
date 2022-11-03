aeee487b7ed563de389d75b43c934902d727fd52
EBTCSwap <ebtcswap@qq.com>
2022-11-03 14:16:11 +0800

    first commit

---
+++ core/settings.php
@@ -0,0 +1,27 @@
+<?php
+# @*************************************************************************@
+# @ Software author: Mansur Altamirov (Mansur_TL)							@
+# @ Author_url 1: https://www.instagram.com/mansur_tl                       @
+# @ Author_url 2: http://codecanyon.net/user/mansur_tl                      @
+# @ Author E-mail: vayart.help@gmail.com                                    @
+# @*************************************************************************@
+# @ ColibriSM - The Ultimate Modern Social Media Sharing Platform           @
+# @ Copyright (c) 2020 - 2021 ColibriSM. All rights reserved.               @
+# @*************************************************************************@
+
+
+# MySQL db host name
+$sql_db_host = "127.0.0.1";
+
+# MySQL db User
+$sql_db_user = "ebtcswap_co";
+
+# MySQL db Password
+$sql_db_pass = "1111";
+
+# MySQL db Name
+$sql_db_name = "ebtcswap_co";
+
+# Site URL
+$site_url    = "http://192.168.108.200"; // E.g (https://www.mysite.com)
+?>
